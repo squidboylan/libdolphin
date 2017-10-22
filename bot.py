9b89fcc2146d2d984786b24570a49877826fff67
Caleb Boylan <calebboylan@gmail.com>
2017-10-22 11:55:11 -0700

    Read a couple player things from from the GC

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+
 import socket
 import os
 import sys
+import struct
 import subprocess
+import binascii
+import yaml
+
+class MeleeState:
+    def __init__(self):
+        with open("memory.yaml", 'r') as stream:
+            self.memory_meta = yaml.load(stream)
+
+    # Load GC memory and convert to right type and print
+    def process_memory(self, data):
+        p = data[1].strip('\x00').zfill(8)
+        processed_p = struct.unpack(self.memory_meta[data[0]]['type'],
+                binascii.unhexlify(p))[int(self.memory_meta[data[0]]['elem'])]
+        print(self.memory_meta[data[0]]['name'] + ": " + str(processed_p))
 
 sock_path = "/home/squid/.local/share/dolphin-emu/MemoryWatcher/MemoryWatcher"
 try:
@@ -17,6 +34,7 @@ sock.bind(sock_path)
 while True:
     try:
         data = sock.recvfrom(9096)[0].decode('utf-8').splitlines()
-        print(data)
+        m = MeleeState()
+        m.process_memory(data)
     except socket.timeout:
         pass
