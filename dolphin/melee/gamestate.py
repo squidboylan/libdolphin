ab64e951d22ddb945843f9c7f0fb0ae00327d36e
Caleb Boylan <calebboylan@gmail.com>
2017-10-22 22:34:07 -0700

    Update to use player data pointers, but locations are wrong

--- dolphin/melee/gamestate.py
+++ dolphin/melee/gamestate.py
@@ -1,5 +1,8 @@
 import socket
 from melee.player import Player
+import struct
+import yaml
+import binascii
 
 class GameState:
     def __init__(self, sock_path):
@@ -8,14 +11,27 @@ class GameState:
         for i in range(4):
             self.players.append(Player(i+1))
 
+        with open("melee/data/globals.yaml", "r") as f:
+            self.global_data_config = yaml.load(f.read())
+
+        self.global_data = {}
+        for i in self.global_data_config.keys():
+            self.global_data[self.global_data_config[i]["name"]] = 0
+
     def sock_bind(self):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
         self.sock.bind(self.sock_path)
 
     def update(self):
         data = self.sock.recvfrom(9096)[0].decode('utf-8').splitlines()
-        for i in range(4):
-            self.players[i].update(data)
+        if data[0] in self.global_data_config.keys():
+            val = data[1].strip('\x00').zfill(8)
+            val = struct.unpack(self.global_data_config[data[0]]['type'],
+                    binascii.unhexlify(val))[self.global_data_config[data[0]]['index']]
+            self.global_data[self.global_data_config[data[0]]['name']] = val
+        else:
+            for i in range(4):
+                self.players[i].update(data)
 
     def print_state(self):
         for i in range(4):
--- dolphin/melee/player.py
+++ dolphin/melee/player.py
@@ -4,37 +4,58 @@ import binascii
 
 class Player:
     def __init__(self, player_num):
+        with open("melee/data/static_player_block.yaml", "r") as f:
+            self.static_block_config_file = yaml.load(f.read())
+
         with open("melee/data/static_player_data.yaml", "r") as f:
-            self.config_from_file = yaml.load(f.read())
+            self.character_data_config_file = yaml.load(f.read())
 
         self.player_num = player_num
+
         self.generate_config_from_offsets()
-        self.data = {}
-        for i in self.static_data_loc.keys():
-            self.data[self.static_data_loc[i]['name']] = 0
+
+        self.static_block_data = {}
+        self.character_data = {}
+
+        for i in self.static_block_config.keys():
+            self.static_block_data[self.static_block_config[i]['name']] = 0
+
+        for i in self.character_data_config.keys():
+            self.character_data[self.character_data_config[i]['name']] = 0
 
     def generate_config_from_offsets(self):
-        start_pos = self.config_from_file['start_pos'][self.player_num]
+        start_pos = self.static_block_config_file['start_pos'][self.player_num]
         start_pos = binascii.unhexlify(start_pos)
-        self.static_data_loc = {}
-        for i in self.config_from_file['offset'].keys():
+        self.static_block_config = {}
+        for i in self.static_block_config_file['offset'].keys():
             padded_i = i.zfill(8)
             i_bin = binascii.unhexlify(padded_i)
             final_pos = start_pos + i_bin
             string_pos = binascii.hexlify(struct.pack(">I", struct.unpack(">I", i_bin)[0] + struct.unpack(">I", start_pos)[0]))
-            self.static_data_loc[string_pos.upper().decode("utf-8")] = self.config_from_file['offset'][i]
+            self.static_block_config[string_pos.upper().decode("utf-8")] = self.static_block_config_file['offset'][i]
+
+        start_pos = self.character_data_config_file['start_pos'][self.player_num]
+        self.character_data_config = {}
+        for i in self.character_data_config_file['offset'].keys():
+            self.character_data_config[start_pos.upper() + " " + i.upper()] = self.character_data_config_file['offset'][i]
 
     def update(self, data):
-        if data[0] in self.static_data_loc.keys():
+        if data[0] in self.static_block_config.keys():
             val = data[1].strip('\x00').zfill(8)
-            val = struct.unpack(self.static_data_loc[data[0]]['type'],
-                    binascii.unhexlify(val))[self.static_data_loc[data[0]]['index']]
-            self.data[self.static_data_loc[data[0]]['name']] = val
+            val = struct.unpack(self.static_block_config[data[0]]['type'],
+                    binascii.unhexlify(val))[self.static_block_config[data[0]]['index']]
+            self.static_block_data[self.static_block_config[data[0]]['name']] = val
+
+        elif data[0] in self.character_data_config.keys():
+            val = data[1].strip('\x00').zfill(8)
+            val = struct.unpack(self.character_data_config[data[0]]['type'],
+                    binascii.unhexlify(val))[self.character_data_config[data[0]]['index']]
+            self.character_data[self.character_data_config[data[0]]['name']] = val
 
     def print_data(self):
-        if self.data['state'] == 2:
+        if self.static_block_data['state'] == 2:
             print("Player: " + str(self.player_num))
-            print(self.data)
+            print(self.character_data)
 
 if __name__ == "__main__":
     player = []
