d02fb8095ec0578186f851c3ebd762b6ead7da9e
Caleb Boylan <calebboylan@gmail.com>
2017-10-24 12:43:52 -0700

    Create MemoryWatcher dir

--- dolphin/dolphin.py
+++ dolphin/dolphin.py
@@ -22,6 +22,11 @@ locations_file_contents = game.generate_locations_file()
 with open(locations_path, 'w') as f:
     f.write(locations_file_contents)
 
+try:
+    os.mkdir(os.path.split(sock_path)[0])
+except FileExistsError:
+    pass
+
 process = subprocess.Popen("dolphin-emu -e /home/squid/.local/share/dolphin-emu/Games/smash-1.02.iso", shell=True)
 game.sock_bind()
 if __name__ == "__main__":
