185a5c914487b586f7f9fdd92dd2169aa2fe1106
Caleb Boylan <calebboylan@gmail.com>
2017-11-08 21:46:55 -0800

    Add more management of controller configs

--- libdolphin/dolphin.py
+++ libdolphin/dolphin.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+import copy
 import configparser
 import socket
 import os
 import sys
 import struct
 import subprocess
+from shutil import copyfile
 import time
 import datetime
 import binascii
@@ -17,12 +19,19 @@ import libdolphin.melee.gamestate
 from controller import *
 
 class Dolphin:
-    def __init__(self, game_path=str(Path.home()) + "/.local/share/dolphin-emu/Games/smash-1.02.iso", dolphin_path="dolphin-emu"):
-        self.sock_path = str(Path.home()) + "/.local/share/dolphin-emu/MemoryWatcher/MemoryWatcher"
-        self.locations_path = str(Path.home()) + "/.local/share/dolphin-emu/MemoryWatcher/Locations.txt"
-        self.config_path = str(Path.home()) + "/.config/dolphin-emu/Dolphin.ini"
+    def __init__(self, game_path=str(Path.home()) + "/.local/share/dolphin-emu/Games/smash-1.02.iso", dolphin_path="dolphin-emu", bot_ports = [2], human_ports = [1]):
+        self.bot_ports = bot_ports
+        self.human_ports = human_ports
         self.game_path = game_path
         self.dolphin_path = dolphin_path
+
+        self.dolphin_dir = str(Path.home()) + "/.local/share/dolphin-emu/"
+        self.sock_path = self.dolphin_dir + "/MemoryWatcher/MemoryWatcher"
+        self.locations_path = self.dolphin_dir + "/MemoryWatcher/Locations.txt"
+        self.pipes_path = self.dolphin_dir + "/Pipes/"
+        self.config_dir = str(Path.home()) + "/.config/dolphin-emu/"
+        self.config_path = self.config_dir + "/Dolphin.ini"
+
         try:
             os.unlink(self.sock_path)
         except:
@@ -39,6 +48,7 @@ class Dolphin:
         except FileExistsError:
             pass
 
+    # Configure the dolphin emulator
     def configure(self):
         dolphin_config = configparser.ConfigParser()
         dolphin_config.read(self.config_path)
@@ -51,11 +61,31 @@ class Dolphin:
         with open(self.config_path, 'w') as f:
             dolphin_config.write(f)
 
+        #copyfile(os.path.dirname(__file__) + "/melee/data/GCPadNew.ini", self.config_dir + "/GCPadNew.ini")
+        gcpadnew_config = configparser.ConfigParser()
+        controller_config = configparser.ConfigParser()
+        controller_config.read(os.path.dirname(__file__) + "/melee/data/GCPadNew.ini")
+
+        for i in self.bot_ports:
+            gcpadnew_config['GCPad' + str(i)] = controller_config['GCPad']
+            gcpadnew_config['GCPad' + str(i)]['device'] = "Pipe/0/Bot" + str(i)
+            try:
+                os.mkfifo(self.pipes_path + "Bot" + str(i + 1))
+            except:
+                pass
+
+        for i in self.human_ports:
+            gcpadnew_config['GCPad' + str(i)] = controller_config['Keyboard']
+
+        with open(self.config_dir + "/GCPadNew.ini", 'w') as f:
+            gcpadnew_config.write(f)
+
     def run(self):
         self.configure()
         self.process = subprocess.Popen(self.dolphin_path + " -e " + self.game_path, shell=True)
 
-        self.game.players[1].controller = Controller(str(Path.home()) + "/.local/share/dolphin-emu/Pipes/Bot2")
+        for i in self.bot_ports:
+            self.game.players[i-1].controller = Controller(str(Path.home()) + "/.local/share/dolphin-emu/Pipes/Bot" + str(i))
         self.game.sock_bind()
 
     def next_input(self, frame_diff):
