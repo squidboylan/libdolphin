59992afe7c7b8810b06cab78f709ac60ecfc5dcf
Caleb Boylan <calebboylan@gmail.com>
2017-12-07 22:24:14 -0800

    Clean up menu helper functions

--- libdolphin/melee/menu_helper.py
+++ libdolphin/melee/menu_helper.py
@@ -1,6 +1,7 @@
 import yaml
 import libdolphin
 import os
+import math
 
 with open(os.path.dirname(__file__) + "/data/character_select.yaml", "r") as f:
     character_select_data = yaml.load(f.read())
@@ -8,39 +9,46 @@ with open(os.path.dirname(__file__) + "/data/character_select.yaml", "r") as f:
 with open(os.path.dirname(__file__) + "/data/character_select_options.yaml", "r") as f:
     character_select_options = yaml.load(f.read())
 
-# THIS IS A HACK AND NEEDS TO BE CHANGED TO MAKE IT PERMANENT, BUT IT MAKES IT
-# EASIER TO TEST IF IT EXISTS AS A HACK FOR NOW
 def select_character(game, character, player):
-    controller = player.controller
-    x = character_select_data[character]['x']
-    y = character_select_data[character]['y']
-    x_diff = x - game.global_data['p' + str(player.player_num) + '_cursor_x']
-    y_diff = y - game.global_data['p' + str(player.player_num) + '_cursor_y']
+    if player.character_selected == False:
+        controller = player.controller
+        x = character_select_data[character]['x']
+        y = character_select_data[character]['y']
+        x_diff = x - game.global_data['p' + str(player.player_num) + '_cursor_x']
+        y_diff = y - game.global_data['p' + str(player.player_num) + '_cursor_y']
 
-    if abs(x_diff) >= 1.24:
-        if x_diff > 0:
-            x_vel = .65
-        elif x_diff < 0:
-            x_vel = .35
-    else:
-        x_vel = 0.5
+        if math.sqrt(abs(x_diff)**2 + abs(y_diff)**2) > 7:
+            mod = .4
+        else:
+            mod = .2
 
-    if abs(y_diff) >= 1.24:
-        if y_diff < 0:
-            y_vel = .35
-        elif y_diff > 0:
-            y_vel = .65
-    else:
-        y_vel = 0.5
+        angle = math.atan(y_diff/x_diff)
 
-    if abs(x_diff) >= 1.24 or abs(y_diff) >=1.24:
-        controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
-                x_vel, y_vel, 1)
+        if x_diff > 0 and y_diff > 0:
+            x_vel = 0.5 + math.cos(angle) * mod
+            y_vel = 0.5 + math.sin(angle) * mod
 
-    else:
-        if player.character_selected == False:
+        elif x_diff < 0 and y_diff > 0:
+            x_vel = 0.5 + -(math.cos(angle) * mod)
+            y_vel = 0.5 + -(math.sin(angle) * mod)
+
+        elif x_diff > 0 and y_diff < 0:
+            x_vel = 0.5 + math.cos(angle) * mod
+            y_vel = 0.5 + math.sin(angle) * mod
+
+        elif x_diff < 0 and y_diff < 0:
+            x_vel = 0.5 + -(math.cos(angle) * mod)
+            y_vel = 0.5 + -(math.sin(angle) * mod)
+
+        if abs(x_diff) > 1.24 or abs(y_diff) > 1.24:
+            controller.empty_queue()
             controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
-                    x_vel, y_vel, 1)
+                    x_vel, y_vel, 0)
+
+        else:
+            controller.empty_queue()
+            controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
+                    0.5, 0.5, 0)
             controller.press_button(libdolphin.controller.Buttons.A.value,
                     libdolphin.controller.Buttons.press.value, 1)
             controller.press_button(libdolphin.controller.Buttons.A.value,
@@ -48,50 +56,73 @@ def select_character(game, character, player):
             player.character_selected = True
 
 def change_to_cpu(game, player):
-    controller = player.controller
-    x = character_select_options['p' + str(player.player_num)]['type_change']['x']
-    y = character_select_options['p' + str(player.player_num)]['type_change']['y']
-    x_diff = x - game.global_data['p' + str(player.player_num) + '_cursor_x']
-    y_diff = y - game.global_data['p' + str(player.player_num) + '_cursor_y']
+    if player.set_cpu == False:
+        controller = player.controller
+        x = character_select_options['p' + str(player.player_num)]['type_change']['x']
+        y = character_select_options['p' + str(player.player_num)]['type_change']['y']
+        x_diff = x - game.global_data['p' + str(player.player_num) + '_cursor_x']
+        y_diff = y - game.global_data['p' + str(player.player_num) + '_cursor_y']
 
-    if abs(x_diff) >= 1.24:
-        if x_diff > 0:
-            x_vel = .35
-        elif x_diff < 0:
-            x_vel = .65
-    else:
         x_vel = 0.5
-
-    if abs(y_diff) >= 1.24:
-        if y_diff < 0:
-            y_vel = .35
-        elif y_diff > 0:
-            y_vel = .65
-    else:
         y_vel = 0.5
 
-    controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
-            x_vel, y_vel, 1)
+        if math.sqrt(abs(x_diff)**2 + abs(y_diff)**2) > 7:
+            mod = .2
+        else:
+            mod = .1
 
-    if abs(x_diff) < 1.24 and abs(y_diff) < 1.24:
-        if player.set_cpu == False:
+        if y_diff != 0 and x_diff != 0:
+            angle = math.atan(y_diff/x_diff)
+
+        elif y_diff == 0 and x_diff != 0:
+            if x_diff > 0:
+                x_vel = 0.5 + mod
+            else:
+                x_vel = 0.5 - mod
+
+        elif x_diff == 0 and y_diff != 0:
+            if y_diff > 0:
+                y_vel = 0.5 + mod
+            else:
+                y_vel = 0.5 - mod
+
+        if x_diff > 0 and y_diff > 0:
+            x_vel = 0.5 + math.cos(angle) * mod
+            y_vel = 0.5 + math.sin(angle) * mod
+
+        elif x_diff < 0 and y_diff > 0:
+            x_vel = 0.5 + -(math.cos(angle) * mod)
+            y_vel = 0.5 + -(math.sin(angle) * mod)
+
+        elif x_diff > 0 and y_diff < 0:
+            x_vel = 0.5 + math.cos(angle) * mod
+            y_vel = 0.5 + math.sin(angle) * mod
+
+        elif x_diff < 0 and y_diff < 0:
+            x_vel = 0.5 + -(math.cos(angle) * mod)
+            y_vel = 0.5 + -(math.sin(angle) * mod)
+
+        player.controller.empty_queue()
+        controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
+                x_vel, y_vel, 0)
+
+        if abs(x_diff) < 1.24 and abs(y_diff) < 1.24:
             player.controller.empty_queue()
-            print("set_cpu")
+            controller.set_stick(libdolphin.controller.Buttons.main_stick.value,
+                    0.5, 0.5, 0)
             if game.global_data['p' + str(player.player_num) + '_char_mode'] == 0:
-                print("mode 0")
                 controller.press_button(libdolphin.controller.Buttons.A.value,
-                        libdolphin.controller.Buttons.press.value, 4)
+                        libdolphin.controller.Buttons.press.value, 1)
                 controller.press_button(libdolphin.controller.Buttons.A.value,
                         libdolphin.controller.Buttons.release.value, 1)
 
             elif game.global_data['p' + str(player.player_num) + '_char_mode'] == 3:
-                print("mode 3")
                 controller.press_button(libdolphin.controller.Buttons.A.value,
-                        libdolphin.controller.Buttons.press.value, 4)
+                        libdolphin.controller.Buttons.press.value, 1)
                 controller.press_button(libdolphin.controller.Buttons.A.value,
                         libdolphin.controller.Buttons.release.value, 1)
                 controller.press_button(libdolphin.controller.Buttons.A.value,
-                        libdolphin.controller.Buttons.press.value, 4)
+                        libdolphin.controller.Buttons.press.value, 1)
                 controller.press_button(libdolphin.controller.Buttons.A.value,
                         libdolphin.controller.Buttons.release.value, 1)
             player.set_cpu = True
